00f6ddfb871f7f64bd48edc50b995ccd580c8247
MoGHenry <moghenry0@gmail.com>
2024-09-04 13:57:46 -0400

    Revert "changed the positions of the function(played the part as a blue player)"
    This reverts commit 64c2d097f268880723e595406c435b977b531046.

--- calculator.js
+++ calculator.js
@@ -1,19 +1,17 @@
-// Function to multiply two numbers
-function multiply(a, b) {
-  // return a * b;
-}
-
 // Function to subtract two numbers
 function subtract(a, b) {
     // return a - b;
 }
-
 
 // Function to add two numbers
 function add(a, b) {
   // return a + b;
 }
 
+// Function to multiply two numbers
+function multiply(a, b) {
+  // return a * b;
+}
 
 // Function to divide two numbers
 function divide(a, b) {
