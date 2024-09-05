3d64b71fd21dce5d14365f980741a74de743b890
aldstr <aldenstrafford@gmail.com>
2024-09-05 17:37:50 -0400

    subtraction branch init

--- calculator.js
+++ calculator.js
@@ -1,8 +1,9 @@
 // Function to subtract two numbers
 function subtract(a, b) {
   // return a - b;
-}// Function to add two numbers
+}
 
+// Function to add two numbers
 function add(a, b) {
   // return a + b;
 }
