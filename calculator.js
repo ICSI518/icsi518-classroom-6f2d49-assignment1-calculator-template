29f56e10d398412bb993b234dcf3c43c79bcc478
Rahul Tallapaneni <rahultallapaneni@gmail.com>
2024-09-07 10:34:40 -0400

    modified addition function

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 // Function to add two numbers
 function add(a, b) {
-   return a + b; //It will return the sum
+   return a + b; //It will return the sum of two numbers
 }
 
 // Function to subtract two numbers
