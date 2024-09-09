c674531d0448682ccbd656a8d9be1e6dc1afb731
tarunmed001 <tarunmed001@gmail.com>
2024-09-09 19:00:36 +0530

    Update calculator.js
    modifed addition code

--- calculator.js
+++ calculator.js
@@ -1,8 +1,3 @@
-// Function to add two numbers
-function add(a, b) {
-   return a + b; //returns sum of two numbers a,b.
-}
-  
 // Function to multiply two numbers
 function multiply(a, b) {
   // return a * b;
@@ -11,6 +6,11 @@ function multiply(a, b) {
 // Function to subtract two numbers
 function subtract(a, b) {
   // return a - b;
+}
+
+// Function to add two numbers
+function add(a, b) {
+   return a + b; //returns sum of two numbers a,b.
 }
 
 // Function to divide two numbers
