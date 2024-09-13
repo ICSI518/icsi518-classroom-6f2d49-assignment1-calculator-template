0180102da2b21b947ae88ef44bd1949a725723a9
Shreyash Mungilwar <shreyashmungilwar@gmail.com>
2024-09-13 10:36:46 -0400

    Moved Multipled method

--- calculator.js
+++ calculator.js
@@ -1,3 +1,8 @@
+// Function to multiply two numbers
+function multiply(a, b) {
+  // return a * b;
+}
+
 // Function to subtract two numbers
 function subtract(a, b) {
   // return a - b;
@@ -8,10 +13,7 @@ function add(a, b) {
   // return a + b;
 }
 
-// Function to multiply two numbers
-function multiply(a, b) {
-  // return a * b;
-}
+
 
 // Function to divide two numbers
 function divide(a, b) {
