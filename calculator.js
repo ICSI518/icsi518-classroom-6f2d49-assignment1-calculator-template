0f208e3bff8296f4565ebba7524603281c21b41c
Varun Jose <varunjose@Varuns-MBP.lan>
2024-09-14 12:31:51 -0400

    Encountering Merge Conflict

--- calculator.js
+++ calculator.js
@@ -1,11 +1,11 @@
-// Function to add two numbers
-function add(a, b) {
-  // return a + b;
-}
-
 // Function to subtract two numbers
 function subtract(a, b) {
   // return a - b;
+}
+
+// Function to add two numbers
+function add(a, b) {
+  // return a + b;
 }
 
 // Function to multiply two numbers
