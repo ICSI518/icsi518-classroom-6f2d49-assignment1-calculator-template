10fae36ba3a959ec53c25ab05313d8489ca343b7
Tejayenduri9 <118876848+Tejayenduri9@users.noreply.github.com>
2024-09-15 10:46:39 -0400

    Updated subtract to subtraction

--- calculator.js
+++ calculator.js
@@ -2,16 +2,14 @@
 function add(a, b) {
   // return a + b;
 }
-
-function subtract(a, b) {
+//Function to subtract two numbers
+function subtraction(a, b) {
   return a - b;
 }
 function multiply(a, b) {
   // return a * b;
 
 }
-
-// Function to add two numbers
 
 
 // Function to divide two numbers
@@ -28,6 +26,6 @@ const b = 5;
 
 // Display results on console
 console.log(`Addition: ${a} + ${b} = ${add(a, b)}`);
-console.log(`Subtraction: ${a} - ${b} = ${subtract(a, b)}`);
+console.log(`Subtraction: ${a} - ${b} = ${subtraction(a, b)}`);
 console.log(`Multiplication: ${a} * ${b} = ${multiply(a, b)}`);
 console.log(`Division: ${a} / ${b} = ${divide(a, b)}`);
