08c076bbdbeaecc154016fc5fed6a216563552fe
MadhuNangunuri <110600234+MadhuNangunuri@users.noreply.github.com>
2024-09-15 00:46:38 -0400

    Edited the division function

--- calculator.js
+++ calculator.js
@@ -18,10 +18,10 @@ function subtract(a, b) {
 
 // Function to divide two numbers
 function divide(a, b) {
-  // if (b === 0) {
-  //   return "Error: Division by zero";
-  // }
-  // return a / b;
+   if (b === 0) {
+     return "Error: Division by zero";
+   }
+   return a / b;
 }
 
 // Example usage
